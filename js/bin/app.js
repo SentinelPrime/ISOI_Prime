e1e030eaf10d37dcff93472606e8c7485474253c
bhaskarchatterjee28 <bhaskarchatterjee28@gmail.com>
2015-09-19 14:08:44 +0530

    Chapter Description Fixed

--- js/bin/app.js
+++ js/bin/app.js
@@ -1,18 +1,21 @@
     $(document).ready(function(){
-        $('.slider').slider({full_width: true});
-	    $('.slider').slider({height:500});
-	    $('ul.tabs').tabs();
+
+        $('.slider').slider({
+            full_width: true,
+            height:500
+        });
+
         $(".button-collapse").sideNav();
+
         $('.dropdown-button').dropdown({
                 inDuration: 300,
                 outDuration: 225,
-                constrain_width: false, // Does not change width of dropdown to that of the activator
-                hover: true, // Activate on hover
-                gutter: 2, // Spacing from edge
-                belowOrigin: true, // Displays dropdown below the button
-                alignment: 'right' // Displays dropdown with edge aligned to the left of button
-            }
-        );
+                constrain_width: false,
+                hover: true,
+                gutter: 2,
+                belowOrigin: true,
+                alignment: 'right'
+        });
     });
 
 
